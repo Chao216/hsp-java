e371fc3c383b61dfb767a890f13d7a4f17f332af
chao jiang <co.jiaan@proton.me>
2023-06-18 19:58:28 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 19:58:28 CST

---
+++ chapter02_variable/Variable.java
@@ -0,0 +1,13 @@
+// we will use variable to store value
+
+public class Variable {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10086;
+		b = b - a;
+
+		System.out.println(a);
+		System.out.println(a + b);
+		System.out.println(b);
+	}
+}
---
+++ chapter02_variable/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+	public static void main(String[] args) {
+		int i = 1000000;
+		while (i > 0){
+			System.out.println(i);
+			i--;
+		}
+	}
+}
