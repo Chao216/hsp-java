2d9fe5f3a5d1a56a34ae2d1b8d9051cf0edd29f2
chao jiang <co.jiaan@proton.me>
2023-06-18 22:51:29 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 22:51:29 CST

---
+++ chapter02_variable/Char.java
@@ -0,0 +1,23 @@
+public class Char {
+	public static void main(String[] args) {
+		char a = 'A';
+		char b = '超';
+		char c = 109;
+		char e = 351;
+		char f = 352;
+		char g = 353;
+		char d = '\t';
+
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+
+	}
+
+		
+}
