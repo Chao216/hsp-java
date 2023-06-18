454d72dc1b9c7a4dd12ed748cf5564daf5bf5584
chao jiang <co.jiaan@proton.me>
2023-06-18 20:13:47 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 20:13:47 CST

---
+++ chapter02_variable/DataType.java
@@ -0,0 +1,15 @@
+// we will explore different data type here
+
+public class DataType {
+	public static void main(String[] args) {
+		String name = "Chao Jiang";
+		int age = 26;
+		double weight = 68.7;
+		char married = 'N';
+
+		System.out.println(name + " is " + age + " old, and he is " + married + " married " + " and he wwights " + weight);
+	}
+}
+
+// Note ! String  starts with CAPITAL S
+// char  use '' single quataion mark
