10a1a90603254b059c297779a9194fe6d4b50add
chao jiang <co.jiaan@proton.me>
2023-06-18 20:48:30 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 20:48:30 CST

---
+++ chapter02_variable/Bitwise.java
@@ -0,0 +1,19 @@
+// here we will try bitwise operation
+
+public class Bitwise {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		System.out.println(a & b);
+		System.out.println(a | b);
+		System.out.println(a ^ b);
+		
+
+		int c = 10;
+		System.out.println(c);
+		System.out.println(c >> 2);
+		System.out.println(c << 2);
+		
+
+	}
+}
