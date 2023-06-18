e6fa92ff34190e70800b953552c08ab84a6ccc12
chao jiang <co.jiaan@proton.me>
2023-06-18 19:44:09 +0800

    this is a normal regular commit @time

---
+++ README.md
@@ -0,0 +1,2 @@
+Now I will start learning Java 
+2023年 06月 18日 星期日 19:42:08 CST
---
+++ autopush.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+time=`date`
+
+git add *
+git commit -m "this is a normal regular commit @time"
+git push origin main
+
+echo "@time"
