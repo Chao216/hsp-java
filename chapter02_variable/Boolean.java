6da4d1d44ae2be32c96a5fd0b8b2e382bbf8a770
chao jiang <co.jiaan@proton.me>
2023-06-18 23:17:19 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 23:17:19 CST

---
+++ chapter02_variable/Boolean.java
@@ -0,0 +1,12 @@
+// here is a simple taste of Boolean
+
+public class Boolean {
+	public static void main(String[] args) {
+		boolean ispass = true;
+		if (ispass == true) {
+			System.out.println("Congrats, you passed the test !");
+		} else {
+			System.out.println("Don't worry, you can try as many times as you want !");
+		}
+	}
+}
