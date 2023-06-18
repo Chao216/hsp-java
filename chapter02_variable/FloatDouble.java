8e3ec569c84de05157442eec70d262552491659d
chao jiang <co.jiaan@proton.me>
2023-06-18 22:11:28 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 22:11:28 CST

---
+++ chapter02_variable/FloatDouble.java
@@ -0,0 +1,18 @@
+// here we introduce float and double
+// as well as scientific notation
+
+public class FloatDouble {
+	public static void main(String[] args) {
+		float num1 = 1.2f;
+		double num2 = 3.14;
+		float num3 = 1.6e10f;
+		double num4 = 2.79e-10;
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(1.2e3);
+
+	}
+}
