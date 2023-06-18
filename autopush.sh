7117a8e937015d14201f66846c48d482556bb974
chao jiang <co.jiaan@proton.me>
2023-06-18 19:44:57 +0800

    this is a normal regular commit 2023年 06月 18日 星期日 19:44:57 CST

--- autopush.sh
+++ autopush.sh
@@ -3,7 +3,7 @@
 time=`date`
 
 git add *
-git commit -m "this is a normal regular commit @time"
+git commit -m "this is a normal regular commit $time"
 git push origin main
 
-echo "@time"
+echo "$time"
