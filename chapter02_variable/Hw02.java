97b57c1e13219ed3367432567a329be017c738d6
chao jiang <co.jiaan@proton.me>
2023-06-19 20:58:56 +0800

    this is a normal regular commit Mon 19 Jun 2023 08:58:56 PM CST

---
+++ chapter02_variable/Hw02.java
@@ -0,0 +1,18 @@
+public class Hw02 {
+	public static void main(String[] args) {
+		String book1 = "哈利波特";
+		String book2 = "海底两万里";
+		System.out.println(book1 + book2);
+
+		char Author = '东';
+		char Boss = '军';
+
+		System.out.println(Author + Boss);// will return a number
+
+		double price1 = 23.7;
+		double price2 = 29.8;
+		System.out.println(price2 + price1);
+
+	}
+}
+// always use '' for char!!!
