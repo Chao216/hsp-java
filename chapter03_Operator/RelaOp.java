3102a8b38cf6f73ba725af272bff930a3e179b70
chao jiang <co.jiaan@proton.me>
2023-06-20 23:05:01 +0800

    this is a normal regular commit Tue 20 Jun 2023 11:05:01 PM CST

---
+++ chapter03_Operator/RelaOp.java
@@ -0,0 +1,23 @@
+// ==, !=, <, >, <=, >=, instanceof
+
+public class RelaOp {
+	public static void main(String[] args) {
+		System.out.println(1 == 1);
+		System.out.println(1.0 == 1);
+		System.out.println(1.0 != 1);
+		System.out.println(10 > 1);
+		System.out.println(10 < 1);
+		System.out.println(10 >= 10);
+		System.out.println(3 <= 10);
+
+
+		
+		RelaOp test = new RelaOp();
+		System.out.println(test instanceof RelaOp);
+		
+
+
+
+
+	}
+}
