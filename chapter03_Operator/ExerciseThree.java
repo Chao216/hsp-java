0b55841cc69548ad0f2521142f4c60f6eac34b5c
chao jiang <co.jiaan@proton.me>
2023-06-20 22:53:21 +0800

    this is a normal regular commit Tue 20 Jun 2023 10:53:20 PM CST

---
+++ chapter03_Operator/ExerciseThree.java
@@ -0,0 +1,14 @@
+// holiday is coming in 59 days, so how many weeks,and days left?
+
+public class ExerciseThree {
+	public static void main(String[] args) {
+		int w = 59 / 7;
+		int d = 59 % 7;
+		System.out.println("There are " + w + " weeks " + " and " + d + " days left .");
+
+		double f = 234.5;
+
+		double celcius = 5 / 9 * (f - 100);
+		System.out.println("celcius degree = " + celcius);
+	}
+}
