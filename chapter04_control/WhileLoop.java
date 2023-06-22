139193bca5344fa48971ddb0fbe18ee4a41130da
chao jiang <co.jiaan@proton.me>
2023-06-22 22:07:53 +0800

    this is a normal regular commit Thu 22 Jun 2023 10:07:53 PM CST

---
+++ chapter04_control/WhileLoop.java
@@ -0,0 +1,12 @@
+// here is a classic while loop
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		int i = 0;
+
+		while (i <= 20000000) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
