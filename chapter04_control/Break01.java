61b6b3a0fddf521d94c3b9b2c2fbb794682d0c44
chao jiang <co.jiaan@proton.me>
2023-06-23 19:29:02 +0800

    this is a normal regular commit Fri 23 Jun 2023 07:29:02 PM CST

---
+++ chapter04_control/Break01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Break01 {
+	public static void main(String[] args) {
+
+		Scanner myscan = new Scanner(System.in);
+
+		System.out.println("set max number");
+		int max = myscan.nextInt();
+
+		System.out.println("close the sum at what number?");
+		int stop = myscan.nextInt();
+
+
+		int sum = 0;
+		for (int i = 1; i <= max; i++) {
+			sum += i;
+
+			System.out.println("sum is " + sum);
+
+			if (sum == stop) {
+				break;
+			}
+
+		}
+	}
+}
