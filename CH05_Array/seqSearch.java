7eb7d61e9de4fdb0c2e8e23dc4b3c064e4d8e171
chao jiang <co.jiaan@proton.me>
2023-06-29 17:39:51 +0800

    this is a normal regular commit Thu 29 Jun 2023 05:39:51 PM CST

---
+++ CH05_Array/seqSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class seqSearch{
+	public static void main(String[] args) {
+		String[] names = {"Amy", "Tony", "Cynderiala", "Vova", "Apurva", "Leonid", "Chao"};
+		Scanner myScan = new Scanner(System.in);
+
+		System.out.println("who are you looking for?");
+
+		String tgt = myScan.nextLine();// if you want char next().charAt(0);
+
+		boolean found = false;
+
+		for (int i = 0; i < names.length; i++){
+			if (tgt.equals(names[i])){
+
+				System.out.println("yes " + tgt + " is on our customer list!");
+				found = true;
+				break;
+			} 
+			
+		}
+
+
+		if (!found){
+			System.out.println("unfortunately we are not able to find " + tgt);
+		}
+
+
+	}
+}
