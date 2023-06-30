9e3b38fe0b235d6abd7509a773d1a2904fea40eb
chao jiang <co.jiaan@proton.me>
2023-06-30 22:50:58 +0800

    this is a normal regular commit Fri 30 Jun 2023 10:50:58 PM CST

--- CH06_Class_Object/Method01.java
+++ CH06_Class_Object/Method01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Method01{
 	public static void main(String[] args) {
 
@@ -5,7 +7,19 @@ public class Method01{
 		amy.name = "Amy Shelton";
 		amy.age = 12;
 
+
+
 		amy.speak();
+		amy.cal01();
+
+		Scanner myScan = new Scanner(System.in);
+
+		System.out.println("sum up to which number?");
+		int n = myScan.nextInt();
+		amy.cal02(n);
+
+		System.out.println(amy.getSum(10,100));
+		
 		
 	}
 }
@@ -20,5 +34,30 @@ class Person{
 
 	public void speak(){
 		System.out.println("塞车，你做的是什么车? 马自达！");
+	}
+
+	public void cal01(){
+		int sum = 0;
+		for (int i = 1; i < 1001; i++){
+			sum += i;
+		}
+		System.out.println("sum = " + sum);
+	}
+
+	public void cal02(int n){
+		int sum = 0;
+		for (int i = 1; i <= n; i++){
+			sum += i;
+		}
+
+		System.out.println("sum up to " + n + " = " + sum);
+	}
+	// method is public
+	// will return an int
+	// name is getSum
+	// takes 2 arguament int a , int b
+	public int getSum(int a, int b){
+		int res = a + b;
+		return res;
 	}
 }
