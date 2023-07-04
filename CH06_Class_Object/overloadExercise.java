3a65dadbcca68bd9ea5bde78381423fa49c7b148
chao jiang <co.jiaan@proton.me>
2023-07-04 22:55:23 +0800

    this is a normal regular commit Tue 04 Jul 2023 10:55:23 PM CST

--- CH06_Class_Object/overloadExercise.java
+++ CH06_Class_Object/overloadExercise.java
@@ -6,6 +6,10 @@ public class overloadExercise {
 		System.out.println(myMeth.m(10));
 		System.out.println(myMeth.m(10,20));
 		System.out.println(myMeth.m("Hello China!"));
+
+		System.out.println(myMeth.max(10,100));
+		System.out.println(myMeth.max(1.9, 0.13));
+		System.out.println(myMeth.max(1.9, 21.3,0.002));
 		
 	}
 }
@@ -21,5 +25,41 @@ class Methods {
 
 	public String m(String s){
 		return s;
+	}
+
+	public int max(int a, int b){
+		if (a > b){
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	public double max(double a, double b){
+		if (a > b){
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	public double max( double a, double b, double c){
+		if ( a > b ){
+			// a compares c
+
+			if ( a > c){
+				return a;
+			} else {
+				return c;
+			}
+		} else {
+			//b compares c
+
+			if (b > c){
+				return b;
+			} else {
+				return c;
+			}
+		}
 	}
 }
