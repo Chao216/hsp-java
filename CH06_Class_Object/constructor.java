6614d9ab3dea9765f364020ae1eb1ba1edb3a889
chao jiang <co.jiaan@proton.me>
2023-07-05 16:39:36 +0800

    this is a normal regular commit Wed 05 Jul 2023 04:39:36 PM CST

---
+++ CH06_Class_Object/constructor.java
@@ -0,0 +1,32 @@
+public class constructor {
+	public static void main(String[] args) {
+
+		Person per1 = new Person("Valadimir", 29);
+
+		System.out.println(per1.name + "  " + per1.age);
+
+		Person per2 = new Person("Chao");
+
+		System.out.println(per2.name + "  " + per2.age);
+
+		
+	}
+}
+
+class Person {
+	String name;
+	int age;
+
+	// same as method overload, you can create overload for constructor (it is also a method!!!)
+
+	public Person(String pName, int pAge){
+		System.out.println("constructor Person is being executed !");
+		name = pName;
+		age = pAge;
+	}
+
+	public Person(String pName){
+		System.out.println("second constrcutr is being executed!");
+		name = pName;
+	}
+}
