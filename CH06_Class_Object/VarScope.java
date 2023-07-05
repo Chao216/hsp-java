c96d354134183257948752c19b54a8cf1ae6bdde
chao jiang <co.jiaan@proton.me>
2023-07-05 13:32:15 +0800

    this is a normal regular commit Wed 05 Jul 2023 01:32:15 PM CST

---
+++ CH06_Class_Object/VarScope.java
@@ -0,0 +1,45 @@
+// here we will discuss the scope of our variables
+
+public class VarScope {
+	public static void main(String[] args) {
+		
+		Metho meme = new Metho();
+		meme.test1();
+		meme.test2();
+	}
+}
+
+class Metho {
+
+	// outside methods, global variables, all methods can use 
+	int age = 26;
+	boolean rich = true;
+
+	double salary;
+
+	public void test1(){
+
+		// variables defined inside method, cnanot be used outside.
+		int year = 2023;
+
+		String str1 = "Hello world!";
+
+		System.out.println(year);
+		System.out.println(str1);
+	}
+
+	public void test2(){
+		System.out.println(age);
+		System.out.println(rich);
+
+		// System.out.println(year);
+		// System.out.println(str1);
+
+		System.out.println(salary);
+
+		// local variables must be initakized
+
+		// double weight;
+		// System.out.println(weight);
+	}
+}
