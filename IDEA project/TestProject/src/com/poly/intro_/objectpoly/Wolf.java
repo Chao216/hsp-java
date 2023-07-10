4ceb18f23e24e6dccb3aa4ef93ab08e982b42386
chao jiang <co.jiaan@proton.me>
2023-07-10 16:20:16 +0800

    this is a normal regular commit Mon 10 Jul 2023 04:20:16 PM CST

---
+++ IDEA project/TestProject/src/com/poly/intro_/objectpoly/Wolf.java
@@ -0,0 +1,7 @@
+package com.poly.intro_.objectpoly;
+
+public class Wolf extends Animal{
+    public Wolf(String name) {
+        super(name);
+    }
+}
--- IDEA project/TestProject/src/com/poly/intro_/objectpoly/testmorpho.java
+++ IDEA project/TestProject/src/com/poly/intro_/objectpoly/testmorpho.java
@@ -6,5 +6,8 @@ public class testmorpho {
         Grass nzGrass = new Grass("fresh new zealand grass");
         Sheep sheep1 = new Sheep("Shon");
         Chiang.feed(sheep1,nzGrass);
+        Wolf wolf1 = new Wolf("灰太狼");
+        Meat meat1 = new Meat("小肥羊");
+        Chiang.feed(wolf1,meat1);
     }
 }
