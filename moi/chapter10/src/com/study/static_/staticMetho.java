99eb7cb0f738ce6a9f816b63a19e212ab8bfd1cc
chao jiang <co.jiaan@proton.me>
2023-07-17 14:08:58 +0800

    this is a normal regular commit Mon 17 Jul 2023 02:08:58 PM CST

--- moi/chapter10/src/com/study/static_/staticMetho.java
+++ moi/chapter10/src/com/study/static_/staticMetho.java
@@ -7,6 +7,9 @@ public class staticMetho {
         Student tom = new Student("tom");
         tom.pay(300);
         Student.showFee();// static method can be access with CLass.Metho
+        System.out.println(Math.sqrt(100));
+        double[] arr1 = {1.3, 2.5, 0.99, 1.13};
+        System.out.println(mytools.sumArray(arr1));// static method is useful;
 
     }
 }
@@ -26,5 +29,16 @@ class Student {
 
     public static void showFee() {
         System.out.println("we have collected " + Student.fee + "Yuan in total.");
+    }
+}
+
+class mytools {
+    public static double sumArray(double[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+        }
+        return sum;
     }
 }
