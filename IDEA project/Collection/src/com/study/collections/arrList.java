e5cd5f6571f16e60bd8e44a87213f993e9caa4eb
chao jiang <co.jiaan@proton.me>
2023-08-03 14:43:34 +0800

    this is a normal regular commit Thu 03 Aug 2023 02:43:34 PM CST

---
+++ IDEA project/Collection/src/com/study/collections/arrList.java
@@ -0,0 +1,78 @@
+package com.study.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class arrList {
+    public static void main(String[] args) {
+
+        Collection col = new ArrayList();// 向上转型
+        col.add(new Phone("Redmi","K60",2299));
+        col.add(new Phone("Xiaomi","13Pro",5299));
+        col.add(new Phone("Apple","iPhone 11",3599));
+        col.add(new Phone("Samsung","S23",6299));
+
+        Iterator iterator = col.iterator();
+
+        while (iterator.hasNext()) {
+            Object obj =  iterator.next();
+            System.out.println("object is " + obj);
+
+        }
+
+        // if you want to iterate elements again
+        // you need to reset the iterator
+        iterator = col.iterator();
+        while (iterator.hasNext()) {
+            Object next =  iterator.next();
+            System.out.println(next);
+
+        }
+
+
+    }
+}
+
+class Phone {
+    private String brand;
+    private String model;
+    private  double price;
+
+    public Phone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+        return this.brand + " "+ this.model +" "+ this.price+ " $";
+    }
+}
+
