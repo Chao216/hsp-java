8ec36ba888e7139e0d03e1778137c584a51ddf61
chao jiang <co.jiaan@proton.me>
2023-08-14 23:26:35 +0800

    this is a normal regular commit Mon 14 Aug 2023 11:26:35 PM CST

---
+++ IDEA project/Collection/src/com/study/collections/manualLinkedList.java
@@ -0,0 +1,54 @@
+package com.study.collections;
+
+public class manualLinkedList {
+    public static void main(String[] args) {
+
+        Node apple = new Node("Apple");
+        Node banana = new Node("Banana");
+        Node camel = new Node("Camel");
+        apple.next = banana;
+        banana.next=camel;
+        camel.pre = banana;
+        banana.pre=apple;
+
+        Node first = apple;
+        Node last = camel;
+
+        // let us loop through
+
+        while (true){
+            if (first == null){
+                break;
+            }
+            System.out.println(first);
+            first = first.next;
+        }
+
+        System.out.println("now backeards");
+
+        while (true){
+            if (last == null){
+                break;
+            }
+            System.out.println(last);
+            last = last.pre;
+        }
+
+
+    }
+}
+
+class Node {
+    public Object name;
+    public Node next;
+    public Node pre;
+
+    public Node(Object name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Node " + name;
+    }
+}
---
+++ IDEA project/Collection/src/com/study/print/printSomething.java
@@ -0,0 +1,13 @@
+package com.study.print;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class printSomething {
+    public static void main(String[] args) {
+        System.out.println("a");
+
+        List l1 = new ArrayList();
+
+    }
+}
