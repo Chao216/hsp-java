0cfbf78c1c7eb1000d397405965610e0cd9b03a5
chao <chao.c.jiang@outlook.com>
2023-08-18 00:19:51 +0800

    this is a normal regular commit Fri Aug 18 00:19:51 CST 2023

---
+++ IDEA project/Collection/src/com/study/play/nihao.java
@@ -0,0 +1,11 @@
+package com.study.play;
+
+public class nihao {
+    public static void main(String[] args) {
+        System.out.println("nihao xiaopengyou");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
