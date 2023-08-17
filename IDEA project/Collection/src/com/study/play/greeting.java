2f7ad409658632668c53c93c9af5aa008f33761f
chao <chao.c.jiang@outlook.com>
2023-08-18 01:11:55 +0800

    this is a normal regular commit Fri Aug 18 01:11:55 CST 2023

--- IDEA project/Collection/src/com/study/play/greeting.java
+++ IDEA project/Collection/src/com/study/play/greeting.java
@@ -1,8 +1,8 @@
 package com.study.play;
 
-public class nihao {
+public class greeting {
     public static void main(String[] args) {
-        System.out.println("nihao xiaopengyou");
+        System.out.println("greeting xiaopengyou");
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
 
