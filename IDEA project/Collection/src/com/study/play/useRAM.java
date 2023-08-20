ab9aa1ddd38998f576a38e26e9403fce295d0dfa
chao <chao.c.jiang@outlook.com>
2023-08-20 20:02:50 +0800

    this is a normal regular commit Sun Aug 20 20:02:50 CST 2023

---
+++ IDEA project/Collection/src/com/study/play/useRAM.java
@@ -0,0 +1,13 @@
+package com.study.play;
+
+import java.math.BigInteger;
+
+public class useRAM {
+    public static void main(String[] args) {
+        long l1 = 0;
+        while (l1 < 800820880){
+            System.out.println(l1);
+            l1++;
+        }
+    }
+}
