2b3193a6b9d15b47132c1db6ec74a915e718cad0
chao <chao.c.jiang@outlook.com>
2023-08-22 00:05:40 +0800

    this is a normal regular commit Tue Aug 22 00:05:40 CST 2023

--- IDEA project/Collection/src/com/study/File/demo.java
+++ IDEA project/Collection/src/com/study/File/demo.java
@@ -1,7 +1,24 @@
 package com.study.File;
+
 import java.io.File;
+import java.io.IOException;
+
 public class demo {
     public static void main(String[] args) {
-        File newFile = new File("test.txt");
+        try {
+            File newFile = new File("test.txt");
+            if (newFile.createNewFile()) {
+                System.out.println("file " + newFile.getName() + " successfully created!");
+            } else {
+                System.out.println("File " + newFile.getName() + " already existed!");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("this sentence is printed in the finally block");
+        }
+
     }
+
 }
